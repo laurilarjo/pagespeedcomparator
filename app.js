e660fe7a4b613d618342d2e8eae4044a90fd6b22
unknown <ningz@NINGZ-Z220.globalbrain.net>
2015-02-16 12:36:02 +1300

    my first node app.

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/app', function (req, res) {
+  res.send('That is my first Node app.')
+})
+
 var server = app.listen(3000, function () {
 
   var host = server.address().address
