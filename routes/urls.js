30a9621f2d7191a9747cbbb17deee75d93082747
ning Zhou <ning.zhou@sli-systems.com>
2015-02-16 16:08:48 +1300

    app changes

--- routes/urls.js
+++ routes/urls.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var request = require('superagent');
+var fs = require('fs');
+var md5 = require('MD5');
+
 var router = express.Router();
 
 router.post('/speedtesturl', function(req, res, next) {
@@ -18,8 +22,25 @@ router.get('/', function(req, res, next) {
 });
 
 speedtesturl = function(url) {
-	var results = {"Load Time" : 5423, "Activity Time" : 6023};
 
+	//get request to API
+	var inputUrl = 'www.aol.com';
+	//var url='http://www.webpagetest.org/runtest.php?url='+inputUrl+'&k=A.d0c8cb56fabd227b13d616a75e00f021&f=json';
+	//request.get(url, function(result_1){
+		//var summaryFile = result_1.body.data.summaryCSV;
+		//console.log(summaryFile);
+		var summaryFile='http://www.webpagetest.org/result/150216_30_4CG/page_data.csv';
+		request.get(summaryFile, function(result_2){
+			//console.log(result_2);
+			if(result_2){
+				var csvFilePath = './temp/'+md5(inputUrl)+'.csv';
+				fs.writeFile(csvFilePath, result_2.text);
+			}
+			
+		});
+	//})
+	var results='That is my first Node app.';
+  
 	return results;
 }
 
